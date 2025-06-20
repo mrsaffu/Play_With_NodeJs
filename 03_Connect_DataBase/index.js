a1f88d4c9ca55e5810618cfece171c440852b2f0
Safwan Ahmad <innoxentsayyed@gmail.com>
2025-06-21 00:54:29 +0530

    add get operation

--- 03_Connect_DataBase/index.js
+++ 03_Connect_DataBase/index.js
@@ -12,10 +12,10 @@ app.get('/', (req, res) => {
 
 })
 
-app.post('/person', async (req,res) => {
+app.post('/person', async (req, res) => {
     try {
         const data = req.body;  // data come from client and stored req.body
-        console.log("req.body ::",data);
+        console.log("req.body ::", data);
 
         // ! create newPerson document using mongoose model
         let newPerson = new Person(data)
@@ -23,13 +23,29 @@ app.post('/person', async (req,res) => {
         // !save Data to MongoDb
         let response = await newPerson.save()
         console.log("Data Saved");
-        res.status(200).json({message:"Data Saved Sucessfully", data:response})
+        res.status(200).json({ message: "Data Saved Sucessfully", data: response })
 
     } catch (err) {
         console.log(err);
-        res.status(500).json({error:"Internal Server Error"})
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
+
+// ! get all details Details
+app.get('/person', async (req, res) => {
+    try {
+        let data = await Person.find()
+        console.log(data);
+        console.log("data Fetch Sucessfully");
+        res.status(200).json({ message: "Data Fetch SucessFully", data: data })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Interal Server error" })
+    }
+})
+
+
 
 
 app.listen(3000, () => {
