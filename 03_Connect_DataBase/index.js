f06d6708ab4fdd6e9d5f3e222a59203da4f025da
Safwan Ahmad <innoxentsayyed@gmail.com>
2025-06-22 01:46:41 +0530

    add MenuItems schema and done operactions for getand post

--- 03_Connect_DataBase/index.js
+++ 03_Connect_DataBase/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 let app = express()
 const db = require('./db')
 const Person = require('./Models/person')
+const MenuItems = require('./Models/minu')
 app.use(express.json())
 
 
@@ -45,6 +46,40 @@ app.get('/person', async (req, res) => {
     }
 })
 
+
+// ! Menu Items
+
+// ^ Post 
+app.post('/menu', async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(data);
+        let newMenuItems = new MenuItems(data)
+        const response = await newMenuItems.save()
+        console.log("Menu Items Saved ");
+        console.log(response);
+
+        res.status(200).json({message:"Menu Items Saved ", data:response})
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error:"Internal server error"})
+
+    }
+})
+
+// ^ Get
+
+app.get('/menu', async (req,res)=>{
+try{
+        const data = await MenuItems.find()
+    console.log("menu Items get sucessfully :", data);
+    res.status(200).json({message:'Menu Items Fetch Sucessfully', data:data})
+}catch(err){
+    console.log(err);
+    res.status(500).json({error:"Internal Server Error"})
+}
+})
 
 
 
